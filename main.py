e7036eb0330e8f1c8b75ed07e8744ee49d34fc0e
MrDlop <72319098+MrDlop@users.noreply.github.com>
2022-11-22 17:10:33 +0500

    2 task

---
+++ main.py
@@ -0,0 +1,43 @@
+import sys
+import random
+
+from PyQt5 import uic
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtWidgets import QWidget, QApplication, QMainWindow
+
+
+class Example(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('Ui.ui', self)
+        self.do_paint = False
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(300, 300, 350, 450)
+        self.setWindowTitle('Рисование')
+        self.pushButton.clicked.connect(self.paint)
+
+    def paint(self):
+        self.do_paint = True
+        self.repaint()
+
+    def paintEvent(self, event):
+        qp = QPainter()
+        qp.begin(self)
+        self.draw_circle(qp)
+        qp.end()
+
+    def draw_circle(self, qp):
+        if self.do_paint:
+            a = random.randint(10, 200)
+            qp.setBrush(QColor(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
+            qp.drawEllipse(50, 50, a, a)
+            self.do_paint=False
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec())
