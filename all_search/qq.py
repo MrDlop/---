5ba95338fff2ac20e8868a72f9c6723e14221993
MrDlop <72319098+MrDlop@users.noreply.github.com>
2023-03-29 18:05:57 +0500

    Update qq.py

--- all_search/qq.py
+++ all_search/qq.py
@@ -1,106 +1,33 @@
-from io import BytesIO
-import requests
-
-
-def map_for_coords(coords: list[float, float],
-                   scale: float = 2,
-                   type_map: str = "map",
-                   **kwargs) -> BytesIO:
-    """
-    :param coords: object coordinates [x, y]
-    :param scale: scale image (1-4)
-    :param type_map: map/sat/skl/trf
-    :param kwargs: other param for static-maps
-    :return: BytesIO map
-    """
-
-    # search map URL
-    map_api_server = "http://static-maps.yandex.ru/1.x/"
-
-    # params for search in static-maps
-    map_params = {
-        "ll": ','.join(str(i) for i in coords),
-        "l": type_map,
-        "scale": str(scale)
-    }
-    for i in kwargs:
-        map_params[i] = kwargs[i]
-
-    # service call
-    response = requests.get(map_api_server, params=map_params)
-
-    return BytesIO(
-        response.content)
-
-
-def search_name(obj: str,
-                scale: float = 2,
-                type_map: str = "map",
-                point: bool = True) -> BytesIO:
-    """
-    :param point: point for object
-    :param type_map: map/sat/skl/trf
-    :param scale: scale image (1-4)
-    :param obj: name object (for example: "Уфа")
-    :return: BytesIO map
-    """
-
-    # search map URL
+def params(obj):
+    import requests
     geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
 
-    # params for search in geocode-maps
     geocoder_params = {
         "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
         "geocode": obj,
         "format": "json"}
 
-    # service call
     response = requests.get(geocoder_api_server, params=geocoder_params)
 
-    json_response = response.json()
+    if not response:
+        pass
 
-    # cut json response
+    json_response = response.json()
+    print(json_response)
     toponym = json_response["response"]["GeoObjectCollection"][
         "featureMember"][0]["GeoObject"]
-    # coords center
-    toponym_coordinates = toponym["Point"]["pos"]
-    # latitude and longitude
-    toponym_longitude, toponym_latitude = toponym_coordinates.split(" ")
+    toponym_coodrinates = toponym["Point"]["pos"]
+    toponym_longitude, toponym_lattitude = toponym_coodrinates.split(" ")
 
     tp = [list(map(float, toponym['boundedBy']['Envelope'][i].split())) for i in toponym['boundedBy']['Envelope']]
     dx = str(abs(tp[0][0] - tp[1][0]))
     dy = str(abs(tp[0][1] - tp[1][1]))
 
-    # # params for search in static-maps
     map_params = {
+        "ll": ",".join([toponym_longitude, toponym_lattitude]),
         "spn": ",".join([dx, dy]),
+        "l": "map",
+        "scale": "1",
+        "pt": f"{toponym['Point']['pos'].replace(' ', ',')}"
     }
-
-    if point:
-        map_params["pt"] = f"{toponym['Point']['pos'].replace(' ', ',')},round"
-
-    return map_for_coords([toponym_longitude, toponym_latitude],
-                          scale=scale,
-                          type_map=type_map,
-                          **map_params)
-
-
-def address_for_coords(coords: str) -> str:
-    """
-    :param coords: coords object
-    :return: full formatted address
-    """
-    geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
-
-    geocoder_params = {
-        "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
-        "geocode": coords,
-        "format": "json"}
-
-    response = requests.get(geocoder_api_server, params=geocoder_params)
-
-    json_response = response.json()
-
-    toponym = json_response["response"]["GeoObjectCollection"][
-        "featureMember"][0]["GeoObject"]
-    return toponym['metaDataProperty']['GeocoderMetaData']['Address']['formatted']
+    return map_params
